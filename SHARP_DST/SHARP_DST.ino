7050d7b32bd3c199477517a0873408012532663a
kavkaz86 <38288319+kavkaz86@users.noreply.github.com>
2018-04-12 10:36:50 +0800

    Add files via upload

---
+++ SHARP_DST/SHARP_DST.ino
@@ -0,0 +1,26 @@
+#define DELAY 5
+
+float distance=0; 
+
+float val = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+  pinMode(A0, INPUT);
+  
+}
+
+void loop() {
+
+  Serial.println(28250/( analogRead(A0) - 229.5));
+
+  delay(DELAY);
+
+  
+
+}
